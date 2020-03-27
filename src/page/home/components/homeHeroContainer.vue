42749a65548b7874d2177afac3bf59737d9da8cb
phdgo <hikaso@163.com>
2020-03-27 08:18:24 +0800

    origin site header completed

--- src/page/home/components/homeHeroContainer.vue
+++ src/page/home/components/homeHeroContainer.vue
@@ -30,6 +30,65 @@
       <div class="swiper-button-prev" slot="button-prev"></div>
       <div class="swiper-button-next" slot="button-next"></div>
     </swiper>
+    <div class="home-hero-sub clearfix">
+      <div class="span4">
+        <ul class="home-channel-list clearfix">
+          <li>
+            <a href="">
+              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/82abdba456e8caaea5848a0cddce03db.png?w=48&h=48" alt="">
+              小米秒杀
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/806f2dfb2d27978e33fe3815d3851fa3.png?w=48&h=48" alt="">
+              企业团购
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/eded6fa3b897a058163e2485532c4f10.png?w=48&h=48" alt="">
+              F码通道
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/43a3195efa6a3cc7662efed8e7abe8bf.png?w=48&h=48" alt="">
+              米粉卡
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/f4846bca6010a0deb9f85464409862af.png?w=48&h=48" alt="">
+              以旧换新
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/9a76d7636b08e0988efb4fc384ae497b.png?w=48&h=48" alt="">
+              话费充值
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul class="home-promo-list">
+        <li class="first">
+          <a href="">
+            <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/7bc54a61b927dd8c54ddd39a0acf0254.jpg?w=632&h=340" alt="">
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/94c74e01afe50a86c3a87ff030b85781.jpg?w=632&h=340" alt="">
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/816a66edef10673b4768128b41804cae.jpg?w=632&h=340" alt="">
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -132,5 +191,105 @@ export default {
 
 .swiper-button-next:hover {
   background-position: -42px 50%;
+}
+
+.home-hero-sub {
+  width: 1226px;
+  margin-top: 14px;
+}
+
+.home-hero-sub .span4 {
+  float: left;
+}
+
+.home-hero-sub .home-channel-list {
+  width: 228px;
+  padding: 3px;
+  background: #5f5750;
+  font-size: 12px;
+  text-align: center;
+}
+
+.home-hero-sub .home-channel-list li,
+.home-hero-sub .home-promo-list li {
+  float: left;
+}
+
+.home-hero-sub .home-channel-list li {
+  position: relative;
+  width: 70px;
+  height: 82px;
+  padding: 0 3px;
+}
+
+.home-hero-sub .home-channel-list li::before,
+.home-hero-sub .home-channel-list li::after {
+  content: "";
+  position: absolute;
+  background: #665e57;
+}
+
+.home-hero-sub .home-channel-list li::before {
+  top: -1px;
+  left: 6px;
+  width: 64px;
+  height: 1px;
+}
+
+.home-hero-sub .home-channel-list li::after {
+  top: 6px;
+  left: 1px;
+  width: 1px;
+  height: 70px;
+}
+
+.home-hero-sub .home-channel-list a {
+  display: block;
+  padding-top: 18px;
+  color: #fff;
+  opacity: .7;
+  transition: opacity .2s;
+}
+
+.home-hero-sub .home-channel-list a:hover {
+  opacity: 1;
+}
+
+.home-hero-sub .home-channel-list img {
+  display: block;
+  width: 24px;
+  height: 24px;
+  margin: 0 auto 4px;
+}
+
+.home-hero-sub .home-promo-list {
+  float: left;
+  margin-left: 14px;
+}
+
+.home-hero-sub .home-promo-list li {
+  width: 316px;
+  height: 170px;
+  margin-left: 15px;
+  transition: box-shadow .2s linear;
+}
+
+.home-hero-sub .home-promo-list li:hover {
+  box-shadow: 0 15px 30px rgba(0,0,0,.1);
+}
+
+.home-hero-sub .home-promo-list li.first {
+  margin-left: 0;
+}
+
+.home-hero-sub .home-promo-list a {
+  display: block;
+  height: 170px;
+  background: url(//i1.mifile.cn/f/i/2014/cn/placeholder-80.png) no-repeat 50%;
+}
+
+.home-hero-sub .home-promo-list img {
+  width: 316px;
+  height: 170px;
 }
 </style>
